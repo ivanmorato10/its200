2946d099c9de5d12019b8262cb95a98c8852e80f
ivanmorato10 <84759221+ivanmorato10@users.noreply.github.com>
2021-05-30 20:28:54 -0700

    Add files via upload

---
+++ surfer.py
@@ -0,0 +1,19 @@
+#################################################################
+# File name:surf.py                                             #
+# Author: Ivan Morato                                           #
+# Date:5-29-2021                                                #
+# Classes:  ITS 220: Programming Languages & Concepts           #
+# Instructor:Matthew Loschiavo                                  #
+#   Use 3 separate arrays to store the program data             #
+#   (One for the names, the other for the scores,               #
+#   and the last one for their ages).                           #
+#                                                               #
+#################################################################
+
+Names = ['Zack', 'Stacey', 'Juan', 'Joseph', 'Johnny','Aideen','Aaron']
+Score =  [9.12,8.65,8.45,8.31,8.05,7.81,7.21]
+Age = [22,34,33,25,42,27,57]
+
+print(Names[2])
+print (Score[2])
+print (Age[2])
